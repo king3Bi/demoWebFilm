63b133a757a79400084f6eed16abcbfc00b4bbb3
unknown <vovanhauspkt@gmail.com>
2021-08-07 17:58:16 -0700

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const newsRouter = require('./news');
+const siteRouter = require('./site');
+const coursesRouter = require('./courses');
+
+function route(app) {
+
+    app.use('/courses', coursesRouter);
+    app.use('/news', newsRouter);
+    app.use('/', siteRouter);
+
+}
+
+module.exports = route;
